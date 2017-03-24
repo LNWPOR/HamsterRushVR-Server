ccf79f2a5c3cdf747c2728a0a53ce9c631caddc5
LNWPOR <tclnwpor@gmail.com>
2017-03-25 00:45:52 +0700

    use local db

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -1,3 +1,3 @@
 export default {
-	'url' : 'mongodb://LNWPOR:lnwpor@ds149329.mlab.com:49329/hamster_rush_vr' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://localhost:27017/hamsterrushvr' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
